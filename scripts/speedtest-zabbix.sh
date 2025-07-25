a8c5f6e74ae4ed8e3ef823ebe7245937068275a7
Stefan Johner <16019080+sjohner@users.noreply.github.com>
2025-07-25 16:18:05 +0200

    Update speedtest-zabbix.sh
    Changed speedtest command line and modified JSON extract to be compatible with ookla speedtest JSON output.

--- scripts/speedtest-zabbix.sh
+++ scripts/speedtest-zabbix.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-set -e
-
-CACHE_FILE=/tmp/speedtest.tmp
-ZABBIX_DATA=/tmp/speedtest-zabbix.tmp
-LOG_FILE=/var/log/zabbix/speedtest.log
-
 # Create logfile
 touch $LOG_FILE
 chown zabbix:zabbix $LOG_FILE
@@ -12,40 +5,40 @@ chmod 640 $LOG_FILE
 echo | tee -a $LOG_FILE
 echo "$(date +%F-%T)" | tee -a $LOG_FILE
 
-# Randomly choose one of the 10 closest servers
-ID=$(/usr/bin/speedtest-cli --list | head -n 11 | tail -n +2 | cut -f 1 -d ")" | shuf -n1)
+# Set ID of server you want to use for the speed test
+ID=23969
 
 # Gather data
 echo "Running Speedtest"
-/usr/bin/speedtest-cli --server $ID --csv > $CACHE_FILE \
-	| tee -a $LOG_FILE
+json_data=$(/usr/bin/speedtest -s $ID -f json)
 
-# Extract fields
-output=$(cat $CACHE_FILE)
-    WAN_IP=$(echo "$output" | cut -f10 -d ',')
-    PING=$(echo "$output" | cut -f6 -d ',')
-    SRV_NAME=$(echo "$output" | cut -f2 -d ',')
-    SRV_CITY=$(echo "$output" | cut -f3 -d ',')
-    SRV_KM=$(echo "$output" | cut -f5 -d ',' | cut -b1-5)
-    DL=$(echo "$output" | cut -f7 -d ',')
-    UP=$(echo "$output" | cut -f8 -d ',')
+# Extract values using jq and eval
+SRV_NAME=$(echo "$json_data" | jq -r '.server.host')
+DL=$(echo "$json_data" | jq -r '.download.bandwidth')
+UP=$(echo "$json_data" | jq -r '.upload.bandwidth')
+JITTER=$(echo "$json_data" | jq -r '.ping.jitter')
+LATENCY=$(echo "$json_data" | jq -r '.ping.latency')
+ISP=$(echo "$json_data" | jq -r '.isp')
+SRV_CITY=$(echo "$json_data" | jq -r '.server.location')
+WAN_IP=$(echo "$json_data" | jq -r '.interface.externalIp')
 
 # Print some results
-echo "ping: $PING, down: $DL, up: $UP, server: $SRV_NAME" | tee -a $LOG_FILE
+echo "HOST: $SRV_NAME, DOWN: $DL, UP: $UP, JITTER: $JITTER, LATENCY: $LATENCY, ISP: $ISP, SERVER CITY: $SRV_CITY, WAN IP: $WAN_IP" | tee -a $LOG_FILE
 
 # Summarize Data for Zabbix
  echo "-" speedtest.download $DL >> $ZABBIX_DATA 
  echo "-" speedtest.upload $UP >> $ZABBIX_DATA 
  echo "-" speedtest.wan.ip $WAN_IP >> $ZABBIX_DATA 
- echo "-" speedtest.ping $PING >> $ZABBIX_DATA
+ echo "-" speedtest.ping.jitter $JITTER >> $ZABBIX_DATA
+ echo "-" speedtest.ping.latency $LATENCY >> $ZABBIX_DATA
  echo "-" speedtest.srv.name $SRV_NAME >> $ZABBIX_DATA
  echo "-" speedtest.srv.city $SRV_CITY >> $ZABBIX_DATA
- echo "-" speedtest.srv.km $SRV_KM >> $ZABBIX_DATA
+ echo "-" speedtest.isp $ISP >> $ZABBIX_DATA
 
 # Send data to Zabbix
 echo "Sending Data to Zabbix"
-/usr/bin/zabbix_sender --config /etc/zabbix/zabbix_agentd.conf -i $ZABBIX_DATA \
-	| tee -a $LOG_FILE
+/usr/bin/zabbix_sender --config /etc/zabbix/zabbix_agent2.conf -i $ZABBIX_DATA \
+        | tee -a $LOG_FILE
 
 # Clean data
-rm $CACHE_FILE $ZABBIX_DATA
+rm $ZABBIX_DATA
